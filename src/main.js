a78e91bbc14d60fc059e061f0178d877b7cac629
Alwan-ar <muhammadalwanarfianto2013@gmail.com>
2020-06-23 20:58:33 +0700

    shopping

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {store} from './store';
+import VueSweetalert2 from 'vue-sweetalert2';
+
+Vue.use(VueSweetalert2);
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
